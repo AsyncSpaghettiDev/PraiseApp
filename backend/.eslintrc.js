457aeea91cf496dc9614074e78a9100f16fd6ed4
AsyncSpaghettiDev <jafetmv21@gmail.com>
2024-09-13 09:52:14 -0700

    precommit hook added

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'no-extra-semi': 'error',
-    "semi": [ "error", "never" ]
+    "semi": [ "error", "never" ],
+    'no-useless-constructor': 'off',
   },
 };
